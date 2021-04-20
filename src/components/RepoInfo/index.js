f88b4ae96dbfff39ac992ece7312ac6f7350ed5b
HeyAero <aeroyts@icloud.com>
2021-04-20 15:17:02 +0100

    add RepoInfo useState, useParams, useEffect along with a fetchRepo method that makes the GET request

--- src/components/RepoInfo/index.js
+++ src/components/RepoInfo/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const RepoInfo = () => {
+
+  const [repo, setRepo] = useState();
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchRepo = async () => {
+      try {
+        let { data } = await axios.get(`https://api.github.com/repositories/${params.id}`);
+        console.log(data);
+        setRepo(data[0]);
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+    fetchRepo();
+  }, [params])
+
   return (
     <>
+      <h1>Test</h1>
     </>
   )
 }

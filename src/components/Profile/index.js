ab9d788c148be9564eedf51758076730748dd4e4
HeyAero <aeroyts@icloud.com>
2021-04-20 21:49:07 +0100

    update styling on Profile component to be centeral and resized

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -5,11 +5,10 @@ const Profile = (props) => {
 
 
     return(
-        <>
+    <div className="text-center mt-5">
         <h2>{props.repoData.owner.login}</h2>
         <img className="profile-picture" src = {props.repoData.owner.avatar_url}></img>
-
-    </>
+    </div>
     )
 }
 

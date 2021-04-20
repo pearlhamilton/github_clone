ba8dfbdec2743e6042787f1dbf520b4464c3cfb7
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-20 15:14:13 +0100

    add router and stub out homepage

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Home } from './pages'
+
+
+function App() {
+  return (
+    <>
+
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+
+  </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+const Home = () => {
+    return(
+        <h1>I am the homepage</h1>
+
+    )
+
+
+}
+
+export default Home

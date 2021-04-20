0139e0034d2be77cb606e745f88b33841a79f5be
HeyAero <aeroyts@icloud.com>
2021-04-20 16:37:25 +0100

    update RepoInfo title to be a tag so it can link to externaly to github

--- src/components/RepoInfo/index.js
+++ src/components/RepoInfo/index.js
@@ -29,27 +29,27 @@ const RepoInfo = () => {
       <div className="repo-info">
         <div className="repo-header d-flex">
           <img src={repo.owner.avatar_url} alt={repo.owner.login}/>
-          <div className="">
+          <div className="ml-3">
             <h5>{repo.owner.login}</h5>
-            <h1>{repo.name}</h1>
+            <a href={repo.html_url}>{repo.name}</a>
             <p>{repo.description ? repo.description : "No description available."}</p>
           </div>
         </div>
         <div className="repo-stats text-center">
-          <div class="row">
-            <div class="col-sm repo-stat">
+          <div className="row">
+            <div className="col-sm repo-stat">
               <h5>Forks</h5>
               <p>{repo.forks ? repo.forks : "Unkown"}</p>
             </div>
-            <div class="col-sm repo-stat">
+            <div className="col-sm repo-stat">
               <h5>Open Issues</h5>
               <p>{repo.open_issues ? repo.open_issues : "Unkown"}</p>
             </div>
-            <div class="col-sm repo-stat">
+            <div className="col-sm repo-stat">
               <h5>Watchers</h5>
               <p>{repo.watchers ? repo.watchers : "Unkown"}</p>
             </div>
-            <div class="col-sm repo-stat">
+            <div className="col-sm repo-stat">
               <h5>Subscribers</h5>
               <p>{repo.subscribers_count ? repo.subscribers_count : "Unkown"}</p>
             </div>

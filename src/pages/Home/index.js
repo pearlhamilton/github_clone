bfc6b63805d18d83c55756b89c9f17ec799a50fa
HeyAero <aeroyts@icloud.com>
2021-04-20 22:16:25 +0100

    fix issue where you could not go from a user to a 404 error

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,6 +16,7 @@ const Home = () => {
             setError();
         } catch (err) {
             console.warn(err);
+            setRepos()
             setError("User Does not Exist");
         }
     }
